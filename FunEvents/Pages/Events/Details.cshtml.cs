26c86b3715919b814e0c60629b54a223ee1aeda6
Robin Grabs <robinmgrabs@gmail.com>
2021-03-30 18:24:01 +0200

    Sorting for events. Join Events working straight from index list

--- FunEvents/Pages/Events/Details.cshtml.cs
+++ FunEvents/Pages/Events/Details.cshtml.cs
@@ -2,15 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FunEvents.Data;
+using FunEvents.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace FunEvents.Pages.Events
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ActiveUser> _userManager;
+        private readonly SignInManager<ActiveUser> _signInManager;
+
+        public DetailsModel(ApplicationDbContext context,
+           UserManager<ActiveUser> userManager,
+           SignInManager<ActiveUser> signInManager)
         {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public ActiveUser ActiveUser { get; set; }
+        public Event EventToJoin { get; set; }
+        public async Task OnGetAsync(int? id)
+        {
+            string userId = _userManager.GetUserId(User);
+            ActiveUser = await _context.Users.Where(u => u.Id == userId).Include(u => u.MyEvents).FirstOrDefaultAsync();
+            EventToJoin = await _context.Events.Where(e => e.Id == id).FirstOrDefaultAsync();
+            ActiveUser.MyEvents.Add(EventToJoin);
+            await _context.SaveChangesAsync();
         }
     }
 }
--- FunEvents/Pages/Events/MyEvents.cshtml.cs
+++ FunEvents/Pages/Events/MyEvents.cshtml.cs
@@ -6,23 +6,40 @@ using FunEvents.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using FunEvents.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FunEvents.Pages.Events
 {
     public class MyEventsModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ActiveUser> _userManager;
+        private readonly SignInManager<ActiveUser> _signInManager;
 
-        public MyEventsModel(ApplicationDbContext context)
+        public MyEventsModel(ApplicationDbContext context,
+           UserManager<ActiveUser> userManager,
+           SignInManager<ActiveUser> signInManager)
         {
             _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [BindProperty]
         public ActiveUser ActiveUser { get; set; }
 
-        public void OnGet()
+        public List<Event> Events { get; set; }
+
+        public async Task OnGetAsync()
         {
+            string userId = _userManager.GetUserId(User);
+
+            ActiveUser = await _context.Users.Where(u => u.Id == userId).Include(u => u.MyEvents).FirstOrDefaultAsync();
+
+            Events = await _context.Events.Where(e => e.Attendees.Contains(ActiveUser)).ToListAsync();
+
+            Console.WriteLine(ActiveUser);
         }
     }
 }
