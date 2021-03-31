cd4302e55168e35e69b50fb89feec76f46e584d2
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-03-31 13:35:49 +0200

    edit event page onget (active user som tillfällig organizer)

--- FunEvents/Pages/Events/EditEvent.cshtml.cs
+++ FunEvents/Pages/Events/EditEvent.cshtml.cs
@@ -2,15 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FunEvents.Data;
+using FunEvents.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FunEvents.Pages.Events
 {
     public class EditEventModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ActiveUser> _userManager;
+
+        public EditEventModel(ApplicationDbContext context, UserManager<ActiveUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+        }
+        public ActiveUser ActiveUser { get; set; }
+        public IList<Event> Events { get; set; }
+        public async Task<IActionResult> OnGet()
+        {
+            string userId = _userManager.GetUserId(User);
+
+            Events = await _context.Events.Where(e => e.Organizer.ActiveUser.Id == userId).ToListAsync();
+
+            return Page();
         }
     }
 }
