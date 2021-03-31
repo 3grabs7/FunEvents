83a471260954748f1eed20288c0fcf5e04312b3e
Robin Grabs <robinmgrabs@gmail.com>
2021-03-31 12:44:48 +0200

    merge

--- FunEvents/Pages/Events/Index.cshtml.cs
+++ FunEvents/Pages/Events/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq.Dynamic.Core;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,9 +13,9 @@ using Microsoft.AspNetCore.Identity;
 
 namespace FunEvents.Pages.Events
 {
+    [AllowAnonymous]
     public class IndexModel : PageModel
     {
-
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ActiveUser> _userManager;
 
