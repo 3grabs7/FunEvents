96ea3e3a59d47bb148f5e80c3abb09496eed1bef
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-03-31 13:13:55 +0200

    Manage roles page

--- FunEvents/Pages/AccountManagement/ManageRoles.cshtml.cs
+++ FunEvents/Pages/AccountManagement/ManageRoles.cshtml.cs
@@ -5,14 +5,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FunEvents.Data;
+using FunEvents.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FunEvents.Pages.AccountManagement
 {
     //[Authorize(Roles = "Administrator")]
     public class ManageRolesModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ActiveUser> _userManager;
+
+        public ManageRolesModel(ApplicationDbContext context, UserManager<ActiveUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public ActiveUser ActiveUser { get; set; }
+        public IList<ActiveUser> Users { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            string userId = _userManager.GetUserId(User);
+
+            ActiveUser = await _context.Users.Where(u => u.Id == userId).SingleOrDefaultAsync();
+            Users = await _context.Users.ToListAsync();
+
+            return Page();
         }
     }
 }
